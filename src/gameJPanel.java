a04ebc7b498ccce87d5601a99e48a4c8b44a02b9
Joe <Joe@192.168.166.9>
2016-03-23 23:34:32 -0400

    added framework for the game loops based on modes. started messing with the sprites for the game.

--- src/gameJPanel.java
+++ src/gameJPanel.java
@@ -1,13 +1,15 @@
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 import javax.swing.*;
 
-public class gameJPanel extends JPanel
+public class gameJPanel extends JPanel 
 {
     private boolean gameStatePaused = false;
     private boolean sound, gameKill = false; // will need to be changed after options panel is added
     JButton bSpeed, bMode, bFlavor;
     int speed, flavors, mode;
     options gameOpt;
-    
+    gameSprite flavor1, flavor2, flavor3, flavor4, flavor5, cone;
     public gameJPanel (options inOpt)
     {   
         super();
@@ -19,10 +21,18 @@ public class gameJPanel extends JPanel
         add(bSpeed);
         add(bFlavor);
         add(bMode);
-        updateButtonText();
+        //updateButtonText();
         System.out.println("game constructed");
+        setLayout(null);
+        
+        // starts game loops
+        switch (mode) {
+            case 1: { gameNormal(); break;}
+            case 2: { gameMarathon(); break; }
+            case 3: { gameSurvival(); break; }
+        }
     }
-
+    
     public void setGamePaused(boolean bool) {
         gameStatePaused = bool;
     }
@@ -52,4 +62,29 @@ public class gameJPanel extends JPanel
     public void gQuit() {
         gameKill = true;
     }
+    
+    //Normal Game Loop
+    public void gameNormal() {
+        int x = 500, y = 500;
+        flavor2 = new gameSprite("images/gameP/flavor2.png");
+        flavor1 = new gameSprite("images/gameP/flavor1.png");
+        cone = new gameSprite("images/gameP/coneCut.png");
+        add(flavor2);
+        add(flavor1);
+        add(cone);
+        cone.setBounds(x, y, cone.getWidth(), cone.getHeight());
+        flavor1.setBounds((x + cone.getHCenter() - flavor1.getHCenter()), y - flavor1.getHeight() + 30, flavor1.getWidth(), flavor1.getHeight());
+        flavor2.setBounds((x + flavor1.getHCenter() - flavor2.getHCenter()), y - - flavor1.getHeight() - flavor2.getHeight() + 30, flavor2.getWidth(), flavor2.getHeight());
+    }
+    
+    //Marathon Game Loop
+    public void gameMarathon() {
+        
+    }
+    
+    //Survival Game Loop
+    public void gameSurvival() {
+        
+    }
 }
+
